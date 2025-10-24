505ccb451c67dfe21e2d05a1aa983fb920169589
aleex1848 <halycon01@gmail.com>
2025-10-24 14:11:38 +0200

    fix layout

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -12,28 +12,28 @@
     @if($subheading)
         <flux:subheading>{{ $subheading }}</flux:subheading>
     @endif
-</div>
 
-<div class="flex items-start max-md:flex-col mt-5">
-    <div class="mr-10 w-full pb-4 md:w-[220px]">
-        <flux:navlist>
-            @foreach($navItems as $navItem)
-                @if(!isset($navItem['permission']) || auth()->user()->can($navItem['permission']))
-                    <flux:navlist.item 
-                        :href="$navItem['href']" 
-                        wire:navigate
-                    >
-                        {{ $navItem['label'] }}
-                    </flux:navlist.item>
-                @endif
-            @endforeach
-        </flux:navlist>
-    </div>
+    <div class="flex items-start max-md:flex-col mt-5">
+        <div class="mr-10 w-full pb-4 md:w-[220px]">
+            <flux:navlist>
+                @foreach($navItems as $navItem)
+                    @if(!isset($navItem['permission']) || auth()->user()->can($navItem['permission']))
+                        <flux:navlist.item 
+                            :href="$navItem['href']" 
+                            wire:navigate
+                        >
+                            {{ $navItem['label'] }}
+                        </flux:navlist.item>
+                    @endif
+                @endforeach
+            </flux:navlist>
+        </div>
 
-    <flux:separator class="md:hidden" />
+        <flux:separator class="md:hidden" />
 
-    <div class="flex-1 self-stretch max-md:pt-6">
-        {{ $slot }}
+        <div class="flex-1 self-stretch max-md:pt-6">
+            {{ $slot }}
+        </div>
     </div>
 </div>
 
